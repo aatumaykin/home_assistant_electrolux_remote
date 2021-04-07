a0a3277e2d04ec0d1d42fd67ed2bdaff235e10f9
Александр Тумайкин <AATumaykin@tsum.ru>
2021-04-07 23:42:29 +0300

    feat: refactoring config_flow

--- custom_components/electrolux_remote/config_flow.py
+++ custom_components/electrolux_remote/config_flow.py
@@ -3,76 +3,72 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant import config_entries, core
+from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
-from .const import DOMAIN, HOST_RUSKLIMAT, APPCODE_ELECTROLUX
-from .exception import InvalidHost, CannotConnect, InvalidAuth, UserNotFound
+from .const import DOMAIN, HOST_RUSKLIMAT, APPCODE_ELECTROLUX, CONF_APPCODE
 from .api import RusclimatApi
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_USERNAME): str,
-    vol.Required(CONF_PASSWORD): str,
-    vol.Optional(CONF_HOST, default=HOST_RUSKLIMAT): str,
-    vol.Optional("appcode", default=APPCODE_ELECTROLUX): str,
-})
 
-
-async def validate_input(hass: core.HomeAssistant, data: dict):
-    """Validate the user input allows us to connect."""
-    if len(data["host"]) < 3:
-        raise InvalidHost
-
-    session = async_create_clientsession(hass)
-    api = RusclimatApi(
-        data["host"],
-        data["username"],
-        data["password"],
-        data["appcode"],
-        session
-    )
-    await api.login()
-
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow."""
 
     VERSION = 1
-
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     def __init__(self):
         """Initialize."""
-        _LOGGER.debug("ConfigFlow.init")
+        self._errors = {}
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        _LOGGER.debug("ConfigFlow.async_step_user")
+        """Handle a flow initialized by the user."""
+        self._errors = {}
 
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+        if user_input is not None:
+            valid = await self._test_credentials(
+                user_input[CONF_HOST],
+                user_input[CONF_USERNAME],
+                user_input[CONF_PASSWORD],
+                user_input[CONF_APPCODE],
             )
+            if valid:
+                return self.async_create_entry(
+                    title=user_input[CONF_USERNAME], data=user_input
+                )
+            else:
+                self._errors["base"] = "auth"
+            return await self._show_config_form(user_input)
+        return await self._show_config_form(user_input)
 
-        errors = {}
-
-        try:
-            await validate_input(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
-        except (InvalidAuth, UserNotFound):
-            errors["base"] = "invalid_auth"
-        except Exception:  # pylint: disable=broad-except
-            errors["base"] = "unknown"
-        else:
-            return self.async_create_entry(title="Electrolux remote", data=user_input)
-
+    async def _show_config_form(self, user_input):  # pylint: disable=unused-argument
+        """Show the configuration form to edit location data."""
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_USERNAME): str,
+                vol.Required(CONF_PASSWORD): str,
+                vol.Optional(CONF_HOST, default=HOST_RUSKLIMAT): str,
+                vol.Optional(CONF_APPCODE, default=APPCODE_ELECTROLUX): str,
+            }),
+            errors=self._errors,
         )
 
-
-
+    async def _test_credentials(self, host: str, username: str, password: str, appcode: str):
+        """Return true if credentials is valid."""
+        try:
+            session = async_create_clientsession(self.hass)
+            client = RusclimatApi(
+                host,
+                username,
+                password,
+                appcode,
+                session
+            )
+            await client.login()
+            return True
+        except Exception:  # pylint: disable=broad-except
+            pass
+        return False
--- custom_components/electrolux_remote/const.py
+++ custom_components/electrolux_remote/const.py
@@ -1,6 +1,7 @@
 """Constants for the Electrolux integration."""
 
 DOMAIN = "electrolux_remote"
+CONF_APPCODE = "appcode"
 
 APPCODE_ELECTROLUX = "electrolux"
 HOST_RUSKLIMAT = "http://dongle.rusklimat.ru"
